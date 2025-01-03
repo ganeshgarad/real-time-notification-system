ede47841fff2deb42de0badd133b7749476865c3
Ganesh Garad <ganesh2511995@gmail.com>
2025-01-03 13:14:37 +0530

    Generated Spring Boot project

---
+++ real-time-notification-system/src/main/java/com/spring/realtimenotification/RealTimeNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.realtimenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeNotificationSystemApplication.class, args);
+	}
+
+}
